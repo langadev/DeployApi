21b25dd572d79bce399eddf7ea3e30031eff4b7b
Alfredo Valente Langa <alfredolanga360@gmail.com>
2024-09-30 12:28:27 +0200

    directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/server.js', 
+  entry: './server.js', 
   output: {
     path: path.resolve(__dirname, 'dist'), 
     filename: 'bundle.js', 
